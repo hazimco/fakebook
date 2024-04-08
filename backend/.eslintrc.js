7242c84f966dcb6a9a4adfee1107584f557630c5
hazimco <140416762+hazimco@users.noreply.github.com>
2024-04-09 00:50:52 +0200

    backend: fix: stopping eslint from complaining about jest functions not being defined

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,6 +4,14 @@ module.exports = {
     es2021: true,
   },
   extends: "eslint:recommended",
+  overrides: [
+    {
+      files: ["tests/**/*"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
   parserOptions: {
     ecmaVersion: "latest",
   },
