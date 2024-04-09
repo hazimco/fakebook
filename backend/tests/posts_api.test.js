307ffe214e7b870d84bb8eff190ab939a4cf6795
hazimco <140416762+hazimco@users.noreply.github.com>
2024-04-09 12:25:41 +0200

    backend: adding tests for /api/posts endpoint, step3 - adding POST tests

--- backend/tests/posts_api.test.js
+++ backend/tests/posts_api.test.js
@@ -1,7 +1,10 @@
+const API_PATH = "/api/posts";
+
 const mongoose = require("mongoose");
 const app = require("../app");
 const request = require("supertest");
 const api = request(app);
+const jwt = require("jsonwebtoken");
 
 const helper = require("./posts_api_test_helper");
 
@@ -13,7 +16,7 @@ describe("Endpoint /api/posts", () => {
   describe("GET - Getting posts", () => {
     test("returns correct number of posts", async () => {
       const response = await api
-        .get("/api/posts")
+        .get(API_PATH)
         .expect(200)
         .expect("Content-type", /application\/json/);
 
@@ -22,7 +25,7 @@ describe("Endpoint /api/posts", () => {
 
     test("returns posts with defined properties 'id', 'text', 'createdAt', 'user'", async () => {
       const response = await api
-        .get("/api/posts")
+        .get(API_PATH)
         .expect(200)
         .expect("Content-type", /application\/json/);
 
@@ -36,7 +39,7 @@ describe("Endpoint /api/posts", () => {
 
     test("returns posts that do not contain properties '_id', '__v', 'passwordHash'", async () => {
       const response = await api
-        .get("/api/posts")
+        .get(API_PATH)
         .expect(200)
         .expect("Content-type", /application\/json/);
 
@@ -50,15 +53,153 @@ describe("Endpoint /api/posts", () => {
 
   describe("POST - Adding a new post", () => {
     describe("with valid token", () => {
-      test("succeeds with status 201 when data is valid", () => {});
-      test("fails with status 400 when data is invalid", () => {});
-      test("fails with status 404 when user is not found", () => {});
+      const userForToken = {
+        id: helper.testUser._id,
+        username: helper.testUser.username,
+      };
+      const token = jwt.sign(userForToken, process.env.JWT_SECRET, {
+        expiresIn: "10s",
+      });
+
+      test("succeeds with status 201 when data is valid", async () => {
+        const newPost = { text: "test post that should successfully be added" };
+
+        const response = await api
+          .post(API_PATH)
+          .set("Authorization", `Bearer ${token}`)
+          .send(newPost)
+          .expect(201)
+          .expect("Content-Type", /application\/json/);
+
+        newPost.id = response.body.id;
+        newPost.createdAt = response.body.createdAt;
+        newPost.user = userForToken.id.toString();
+
+        expect(response.body).toStrictEqual(newPost);
+
+        //remove added post to not mess up tests that come later
+        await helper.deletePostFromDb(newPost.id);
+      });
+
+      test("fails with status 400 when text is empty", async () => {
+        const newPostWithEmptyText = { text: "" };
+
+        const response = await api
+          .post(API_PATH)
+          .set("Authorization", `Bearer ${token}`)
+          .send(newPostWithEmptyText)
+          .expect(400)
+          .expect("Content-Type", /application\/json/);
+
+        expect(response.body.error).toBeDefined;
+      });
+
+      test("fails with status 400 when text is too long", async () => {
+        const newPostWithTextTooLong = {
+          text: "This text is too long. How long is too long, you ask? Well currently, it's defined in models/post.js as maxLength 140 characters. That is it.", //141 characters in this text
+        };
+
+        const response = await api
+          .post(API_PATH)
+          .set("Authorization", `Bearer ${token}`)
+          .send(newPostWithTextTooLong)
+          .expect(400)
+          .expect("Content-Type", /application\/json/);
+
+        expect(response.body.error).toBeDefined();
+      });
+
+      test("fails with status 404 when user is not found", async () => {
+        const userForToken = {
+          id: helper.validNonExistingUserId,
+          username: "nonExistingUser",
+        };
+        const token = jwt.sign(userForToken, process.env.JWT_SECRET, {
+          expiresIn: "10s",
+        });
+
+        const response = await api
+          .post(API_PATH)
+          .set("Authorization", `Bearer ${token}`)
+          .send({
+            text: "test post that should fail to be added",
+            user: helper.testUser._id,
+          })
+          .expect(404)
+          .expect("Content-Type", /application\/json/);
+
+        expect(response.body.error).toBe("user not found");
+      });
     });
 
     describe("fails with status 401", () => {
-      test("when token is expired", () => {});
-      test("when token is invalid", () => {});
-      test("when token is missing in authorization header", () => {});
+      test("when token is expired", async () => {
+        const userForToken = {
+          id: helper.testUser._id,
+          username: helper.testUser.username,
+        };
+        const token = jwt.sign(userForToken, process.env.JWT_SECRET, {
+          expiresIn: "0s",
+        });
+        const response = await api
+          .post(API_PATH)
+          .set("Authorization", `Bearer ${token}`)
+          .send(helper.testPosts[0])
+          .expect(401)
+          .expect("Content-Type", /application\/json/);
+
+        expect(response.body.error).toBe("jwt expired");
+
+        const postsInDb = await helper.testPostInDb();
+        expect(postsInDb).toHaveLength(helper.testPosts.length);
+      });
+
+      test("when token is invalid", async () => {
+        const userForTokenWithoutId = {
+          username: helper.testUser.username,
+        };
+        const token = jwt.sign(userForTokenWithoutId, process.env.JWT_SECRET, {
+          expiresIn: "5s",
+        });
+        const response = await api
+          .post(API_PATH)
+          .set("Authorization", `Bearer ${token}`)
+          .send(helper.testPosts[0])
+          .expect(401)
+          .expect("Content-Type", /application\/json/);
+
+        expect(response.body.error).toBe("invalid token");
+
+        const postsInDb = await helper.testPostInDb();
+        expect(postsInDb).toHaveLength(helper.testPosts.length);
+      });
+
+      test("when token is malformed", async () => {
+        const response = await api
+          .post(API_PATH)
+          .set("Authorization", "Bearer malformed-token")
+          .send(helper.testPosts[0])
+          .expect(401)
+          .expect("Content-Type", /application\/json/);
+
+        expect(response.body.error).toBe("jwt malformed");
+
+        const postsInDb = await helper.testPostInDb();
+        expect(postsInDb).toHaveLength(helper.testPosts.length);
+      });
+
+      test("when token is not provided in authorization header", async () => {
+        const response = await api
+          .post(API_PATH)
+          .send(helper.testPosts[0])
+          .expect(401)
+          .expect("Content-Type", /application\/json/);
+
+        expect(response.body.error).toBe("jwt must be provided");
+
+        const postsInDb = await helper.testPostInDb();
+        expect(postsInDb).toHaveLength(helper.testPosts.length);
+      });
     });
   });
 });
--- backend/tests/posts_api_test_helper.js
+++ backend/tests/posts_api_test_helper.js
@@ -5,21 +5,43 @@ if (process.env.NODE_ENV !== "test") {
 const Post = require("../models/post");
 const User = require("../models/user");
 
+const bcrypt = require("bcrypt");
+
 const testUser = new User({
   username: "testUser1",
 });
 
+const validNonExistingUserId = new User()._id;
+
 const testPosts = [
-  { text: "first test post", user: testUser },
-  { text: "second test post", user: testUser },
+  { text: "first test post", user: testUser._id },
+  { text: "second test post", user: testUser._id },
 ];
 
 const initializeTestPosts = async () => {
+  await User.deleteMany({});
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash("123", saltRounds);
+  testUser.passwordHash = passwordHash;
+  await testUser.save();
   await Post.deleteMany({});
   await Post.insertMany(testPosts);
 };
 
+const deletePostFromDb = async (id) => {
+  await Post.deleteOne({ _id: id });
+};
+
+const testPostInDb = async () => {
+  const posts = await Post.find({});
+  return posts.map((post) => post.toJSON());
+};
+
 module.exports = {
   initializeTestPosts,
+  deletePostFromDb,
   testPosts,
+  testPostInDb,
+  testUser,
+  validNonExistingUserId,
 };
