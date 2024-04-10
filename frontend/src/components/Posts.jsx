d9ae4585a6d93cc4267f349d0732fa98e3aa4a3f
hazimco <140416762+hazimco@users.noreply.github.com>
2024-04-10 19:06:33 +0200

    frontend: fetching posts

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,7 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import postsService from "../services/posts";
+
 const Posts = () => {
+  const query = useQuery({
+    queryKey: ["posts"],
+    queryFn: postsService.getAll,
+  });
+
+  if (query.isPending) return <div>loading</div>;
+  if (query.isError) return <div>could not fetch posts</div>;
+
+  const posts = query.data;
+
   return (
     <div>
       <h1>Posts</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          {post.text} by {post.user}
+        </div>
+      ))}
     </div>
   );
 };
