a4be10ecd611c81217b169528c42a3631e9bac8e
hazimco <140416762+hazimco@users.noreply.github.com>
2024-04-11 12:30:03 +0200

    frontend: rendering posts using Post component

---
+++ frontend/src/components/Post.jsx
@@ -0,0 +1,35 @@
+const Post = ({ text, createdAt, username }) => {
+  const postStyle = {
+    container: {
+      border: "solid",
+      borderColor: "lightgray",
+      borderRadius: 6,
+      borderWidth: 1,
+      padding: 20,
+      marginBottom: 10,
+      fontFamily: "Arial",
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: "bold",
+      marginBottom: 15,
+    },
+    text: {
+      fontSize: 18,
+      marginBottom: 20,
+    },
+    createdAt: {
+      fontSize: 12,
+    },
+  };
+
+  return (
+    <div style={postStyle.container}>
+      <div style={postStyle.title}>{username}</div>
+      <div style={postStyle.text}>{text}</div>
+      <div style={postStyle.createdAt}>{createdAt}</div>
+    </div>
+  );
+};
+
+export default Post;
--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import postsService from "../services/posts";
+import Post from "./Post";
 
 const Posts = () => {
   const query = useQuery({
@@ -14,12 +15,20 @@ const Posts = () => {
 
   return (
     <div>
-      <h1>Posts</h1>
-      {posts.map((post) => (
-        <div key={post.id}>
-          {post.text} by {post.user}
-        </div>
-      ))}
+      {/* currently renders with a "blink" when isPending is true. Need to always render the heading */}
+      <h1>Posts</h1>{" "}
+      {posts.map((post) => {
+        console.log("post", post);
+        return (
+          <div key={post.id}>
+            <Post
+              text={post.text}
+              createdAt={post.createdAt}
+              username={post.user.username}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
