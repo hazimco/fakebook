c2286d9f5627194242ce7d113eb00fdc9765c837
hazimco <140416762+hazimco@users.noreply.github.com>
2024-04-18 00:22:29 +0200

    frontend: adding follow/unfollow functions in users service

--- frontend/src/services/users.js
+++ frontend/src/services/users.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import tokenService from "./token";
 const baseUrl = "/api/users";
 
 const getAll = async () => {
@@ -6,6 +7,24 @@ const getAll = async () => {
   return response.data;
 };
 
+const follow = async (data) => {
+  const config = {
+    headers: { Authorization: tokenService.getToken() },
+  };
+  const response = await axios.post(`${baseUrl}/follow`, data, config);
+  return response.data;
+};
+
+const unfollow = async (data) => {
+  const config = {
+    headers: { Authorization: tokenService.getToken() },
+  };
+  const response = await axios.post(`${baseUrl}/unfollow`, data, config);
+  return response.data;
+};
+
 export default {
   getAll,
+  follow,
+  unfollow,
 };
