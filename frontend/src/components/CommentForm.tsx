06a3bd5bfb75e4430c631d0be8a5110c2d69ce53
hazimco <140416762+hazimco@users.noreply.github.com>
2024-05-17 16:02:32 +0200

    frontend: styling CommentForm component

--- frontend/src/components/CommentForm.tsx
+++ frontend/src/components/CommentForm.tsx
@@ -52,17 +52,28 @@ const CommentForm = ({ postId, setShowForm }: Props) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className="text-sm flex-1 mr-2 flex flex-col gap-1"
+    >
       <input
         placeholder="Comment..."
         maxLength={140}
         value={text}
         onChange={(event) => setText(event.target.value)}
+        className="py-1.5 px-2 border-blue-400 border rounded-md"
       />
-      <div>
-        <button>Done</button>
-        <button onClick={closeForm}>Cancel</button>
-        {140 - text.length} characters left
+      <div>{140 - text.length} characters left</div>
+      <div className="flex gap-2">
+        <button className="bg-green-300 hover:bg-green-200 active:bg-green-400 py-1 px-3 rounded-md">
+          Done
+        </button>
+        <button
+          onClick={closeForm}
+          className="bg-red-500 hover:bg-red-400 active:bg-red-600 py-1 px-3 rounded-md text-white"
+        >
+          Cancel
+        </button>
       </div>
     </form>
   );
