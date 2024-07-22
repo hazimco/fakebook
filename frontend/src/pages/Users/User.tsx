929c2b2795aa210cd2d8e488fd0430a58618e348
hazimco <140416762+hazimco@users.noreply.github.com>
2024-07-22 13:06:27 +0200

    frontend: showing error message when following/unfollowing user fails

--- frontend/src/pages/Users/User.tsx
+++ frontend/src/pages/Users/User.tsx
@@ -2,6 +2,9 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import usersService from "../../services/users";
 
 import { User as UserType } from "../../types/types";
+import useNotification from "../../hooks/useNotification";
+import axios from "axios";
+import ErrorNotification from "../../components/ErrorNotification";
 
 interface Props {
   user: UserType;
@@ -14,6 +17,8 @@ const User = ({ user, loggedInUser }: Props) => {
 
   const followedByLoggedInUser = loggedInUser.following.includes(id);
 
+  const [error, setError] = useNotification();
+
   const queryClient = useQueryClient();
 
   const followMutation = useMutation({
@@ -21,12 +26,26 @@ const User = ({ user, loggedInUser }: Props) => {
     onSuccess: (loggedInUserWithAddedFollow) => {
       queryClient.setQueryData(["loggedInUser"], loggedInUserWithAddedFollow);
     },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data.error || error.message);
+      } else {
+        setError("Unknown error: " + error);
+      }
+    },
   });
 
   const unfollowMutation = useMutation({
     mutationFn: usersService.unfollow,
     onSuccess: (loggedInUserWithRemovedFollow) => {
       queryClient.setQueryData(["loggedInUser"], loggedInUserWithRemovedFollow);
+    },
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data.error || error.message);
+      } else {
+        setError("Unknown error: " + error);
+      }
     },
   });
 
@@ -37,19 +56,24 @@ const User = ({ user, loggedInUser }: Props) => {
   };
 
   return (
-    <div className="border-slate-300 border bg-white rounded-md py-2 px-4 mb-2 flex justify-between text-sm">
-      <div className="max-w-52">
-        <div className="font-semibold [word-break:break-word]">{username}</div>
-        <div className="italic font-light">{postCount} posts</div>
+    <div className="border-slate-300 border bg-white rounded-md py-2 px-4 mb-2 text-sm">
+      <div className="flex justify-between">
+        <div className="max-w-52">
+          <div className="font-semibold [word-break:break-word]">
+            {username}
+          </div>
+          <div className="italic font-light">{postCount} posts</div>
+        </div>
+        {loggedInUser.id !== id && (
+          <button
+            onClick={handleClick}
+            className="bg-slate-200 hover:bg-slate-100 active:bg-slate-300 rounded-md ml-6 py-1 px-2 self-center"
+          >
+            {followedByLoggedInUser ? "Unfollow" : "Follow"}
+          </button>
+        )}
       </div>
-      {loggedInUser.id !== id && (
-        <button
-          onClick={handleClick}
-          className="bg-slate-200 hover:bg-slate-100 active:bg-slate-300 rounded-md ml-6 py-1 px-2 self-center"
-        >
-          {followedByLoggedInUser ? "Unfollow" : "Follow"}
-        </button>
-      )}
+      <ErrorNotification message={error} />
     </div>
   );
 };
