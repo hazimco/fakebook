93fca2d5b8c16a6a9108706100469e462d97f565
hazimco <140416762+hazimco@users.noreply.github.com>
2024-10-24 13:45:06 +0200

    backend: adding script to build and copy frontend code into backend folder, and serving it using express.static()

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -28,19 +29,22 @@ morgan.token("body", (req) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(
   morgan(
     "[:date[web]] :remote-addr :method :url :status :res[content-length] - :response-time ms :body"
   )
 );
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 app.use("/api/posts", postsRouter);
+
+// Catch-all route to handle cases when user tries to access specific routes directly,
+// such as reloading page when on /users route.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.use(middleware.errorHandler);
 
