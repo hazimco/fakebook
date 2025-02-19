810b87598c5aae797fca269915ba964b1105a1ab
hazimco <140416762+hazimco@users.noreply.github.com>
2025-02-19 15:34:51 +0100

    frontend: fix: UserCircleIcon was being displayed in front of Menu when using 'scale-110' on icon

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -5,8 +5,10 @@ interface Props {
 }
 
 const Menu = ({ logout }: Props) => {
+  // z-10 added to menu container since using "scale-110" on UserCircleIcon (or any component) leads to the component ending up in front of the menu.
+  // Reason according to GPT: "When you apply a CSS transform (like scale) to an element, it creates a new stacking context. This means that the transformed element and its descendants are rendered in a different layer than the elements that are not transformed."
   return (
-    <div className="bg-slate-50 rounded-b-md pt-4 sticky top-0">
+    <div className="bg-slate-50 rounded-b-md pt-4 sticky top-0 z-10">
       <nav className="bg-blue-500 font-medium text-gray-100 rounded-md p-1 flex gap-4 justify-evenly">
         <StyledNavLink to="/">Profile</StyledNavLink>
         <StyledNavLink to="/posts">Posts</StyledNavLink>
